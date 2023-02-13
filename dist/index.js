26283a1df257d21f7415cbd2f2ad5220e8dd31fe
Madalin Tiutiu <Madalin.Tiutiu@microfocus.com>
2023-02-13 15:13:44 +0200

    17th

--- dist/index.js
+++ dist/index.js
@@ -72388,29 +72388,6 @@ exports.handleEvent = handleEvent;
 
 "use strict";
 
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -72424,17 +72401,19 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core_1 = __nccwpck_require__(42186);
 const github_1 = __nccwpck_require__(95438);
 const eventHandler_1 = __nccwpck_require__(97774);
-const util = __importStar(__nccwpck_require__(47261));
 (() => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const event = github_1.context.payload;
         yield (0, eventHandler_1.handleEvent)(event);
     }
     catch (error) {
-        const msg = error.response
-            ? error.response.data.description_translated
-            : error.message;
-        console.log(util.inspect(error, false, null, true));
+        let msg;
+        if (error.response) {
+            msg = `${error.response.status} - ${error.response.statusText}\nurl:${error.response.config.url} - ${error.response.config.method}\n${error.resopnse.data.description_translated}`;
+        }
+        else {
+            msg = error.message;
+        }
         (0, core_1.setFailed)(msg);
     }
 }))();
@@ -72943,14 +72922,6 @@ module.exports = require("https");
 
 "use strict";
 module.exports = require("net");
-
-/***/ }),
-
-/***/ 47261:
-/***/ ((module) => {
-
-"use strict";
-module.exports = require("node:util");
 
 /***/ }),
 
