2dead8ce2e1d66cbef45ab1510948feddf5cf475
Madalin Tiutiu <Madalin.Tiutiu@microfocus.com>
2023-02-13 12:23:46 +0200

    14th try

--- dist/index.js
+++ dist/index.js
@@ -72007,6 +72007,29 @@ GitHubClient.getCommit = (owner, repo, commitSha) => __awaiter(void 0, void 0, v
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -72024,6 +72047,7 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 const alm_octane_js_rest_sdk_1 = __nccwpck_require__(35063);
 const query_1 = __importDefault(__nccwpck_require__(79406));
 const config_1 = __importDefault(__nccwpck_require__(84561));
+const util = __importStar(__nccwpck_require__(47261));
 class OctaneClient {
     static escapeOctaneQueryValue(q) {
         return (q && q.replace(/\\/g, '\\\\').replace(/\(/g, '\\(').replace(/\)/g, '\\)'));
@@ -72152,7 +72176,7 @@ OctaneClient.getJobBuilds = (jobId) => __awaiter(void 0, void 0, void 0, functio
         .execute()).data;
 });
 OctaneClient.sendScmData = (scmData, instanceId, jobId, buildId) => __awaiter(void 0, void 0, void 0, function* () {
-    yield _a.octane.executeCustomRequest(`/api/shared_spaces/${_a.config.octaneSharedSpace}/scm-commits?instance-id=${instanceId}&job-ci-id=${jobId}&build-ci-id=${buildId}`, alm_octane_js_rest_sdk_1.Octane.operationTypes.update, [scmData]);
+    console.log(util.inspect(yield _a.octane.executeCustomRequest(`/api/shared_spaces/${_a.config.octaneSharedSpace}/scm-commits?instance-id=${instanceId}&job-ci-id=${jobId}&build-ci-id=${buildId}`, alm_octane_js_rest_sdk_1.Octane.operationTypes.update, [scmData]), false, null, true));
 });
 
 
